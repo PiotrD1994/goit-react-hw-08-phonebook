c453848fd24f537fbd1cc23d4df1d75d4baa68b5
Piotr Dubiel <piotrdubiel30@gmail.com>
2023-12-06 17:28:53 +0100

    change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,109 @@
-import { useEffect , lazy} from "react"
-import { useDispatch } from "react-redux"
-import {Route, Routes} from 'react-router-dom'
-import { Layout } from "./Layout/Layout.jsx"
-import { PrivateRoute } from "./PrivateRoute.js"
-import { RestrictedRoute } from "./RestrictedRoute.js"
-import { refreshUser } from "redux/auth/operations.js"
-import { useAuth } from "hooks/useAuth.js"
+import React from "react";
+import { useEffect, lazy, Suspense } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./Layout/Layout.jsx";
+import { PrivateRoute } from "./PrivateRoute.js";
+import { RestrictedRoute } from "./RestrictedRoute.js";
+import { refreshUser } from "redux/auth/operations.js";
+import { useAuth } from "hooks/useAuth.js";
 
-const Home = lazy(() => import('pages/Home'))
-const Register = lazy(() => import('pages/Register'))
-const Login = lazy(() => import('pages/Login'))
-const Contacts = lazy(() => import('pages/Contacts'))
 
-export const App = () => {
-  const dispatch = useDispatch()
-  const {isRefreshing} = useAuth()
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+
+    console.error("Error caught by ErrorBoundary:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <p>Something went wrong. Please try again later.</p>;
+    }
+
+    return this.props.children;
+  }
+}
+
+const Home = lazy(() => import("../pages/Home.jsx"));
+const Register = lazy(() => import("../pages/Register.jsx"));
+const Login = lazy(() => import("../pages/Login.jsx"));
+const Contacts = lazy(() => import("../pages/Contacts.jsx"));
+
+const App = () => {
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
-    dispatch(refreshUser())
-  }, [dispatch])
+    dispatch(refreshUser());
+  }, [dispatch]);
 
-  return isRefreshing ? (
-    <p>User update...</p>
-  ) : (
-    <div>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route
-            path="/register"
-            element={
-              <RestrictedRoute redirectTo="/login" component={<Register />} />
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <RestrictedRoute redirectTo="/contacts" component={<Login />} />
-            }
-          />
-          <Route
-            path="/contacts"
-            element={
-              <PrivateRoute redirectTo="/login" component={<Contacts />} />
-            }
-          />
-        </Route>
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </div>
+  return (
+    <ErrorBoundary>
+      <div>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route
+              index
+              element={
+                <Suspense fallback={<p>Loading...</p>}>
+                  <Home />
+                </Suspense>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <RestrictedRoute
+                  redirectTo="/login"
+                  component={
+                    <Suspense fallback={<p>Loading...</p>}>
+                      <Register />
+                    </Suspense>
+                  }
+                />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute
+                  redirectTo="/contacts"
+                  component={
+                    <Suspense fallback={<p>Loading...</p>}>
+                      <Login />
+                    </Suspense>
+                  }
+                />
+              }
+            />
+            <Route
+              path="/contacts"
+              element={
+                <PrivateRoute
+                  redirectTo="/login"
+                  component={
+                    <Suspense fallback={<p>Loading...</p>}>
+                      <Contacts />
+                    </Suspense>
+                  }
+                />
+              }
+            />
+          </Route>
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </div>
+    </ErrorBoundary>
   );
 };
 
+export default App;
