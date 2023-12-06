b84bced101bcbb4a5e2b219014e12c5db570f69d
Piotr Dubiel <piotrdubiel30@gmail.com>
2023-12-06 17:10:42 +0100

    adding {} to app

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store.js';
 import {PersistGate} from 'redux-persist/integration/react'
 import { BrowserRouter } from 'react-router-dom';
-import App from './components/App.jsx';
+import {App} from './components/App.jsx';
 
 const root = createRoot(document.getElementById('root'));
 
