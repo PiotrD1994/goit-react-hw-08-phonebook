803eeaff2ab8c3018bb0a3025fc1db1144f3debb
Piotr Dubiel <piotrdubiel30@gmail.com>
2023-12-07 16:08:34 +0100

    adding lazy and suspense

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "./Layout/Layout.jsx";
@@ -6,56 +6,54 @@ import { PrivateRoute } from "./PrivateRoute.js";
 import { RestrictedRoute } from "./RestrictedRoute.js";
 import { refreshUser } from "../redux/auth/operations.js";
 
-import Home from "../pages/Home.jsx";
-import Register from "../pages/Register.jsx";
-import Login from "../pages/Login.jsx";
-import Contacts from "../pages/Contacts.jsx";
-
+const Home = lazy(() => import("../pages/Home.jsx"));
+const Register = lazy(() => import("../pages/Register.jsx"));
+const Login = lazy(() => import("../pages/Login.jsx"));
+const Contacts = lazy(() => import("../pages/Contacts.jsx"));
 
 const App = () => {
   const dispatch = useDispatch();
- 
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
   return (
-      <div>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route
-              path="/register"
-              element={
-                <RestrictedRoute
-                  redirectTo="/login"
-                  component={<Register />}
-                />
-              }
-            />
-            <Route
-              path="/login"
-              element={
-                <RestrictedRoute
-                  redirectTo="/contacts"
-                  component={<Contacts />}
-                />
-              }
-            />
-            <Route
-              path="/contacts"
-              element={
-                <PrivateRoute
-                  redirectTo="/login"
-                  component={<Login />}
-                />
-              }
-            />
-          </Route>
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Suspense fallback={<div>Loading...</div>}><Home /></Suspense>} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/login"
+                component={<Suspense fallback={<div>Loading...</div>}><Register /></Suspense>}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<Suspense fallback={<div>Loading...</div>}><Contacts /></Suspense>}
+              />
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute
+                redirectTo="/login"
+                component={<Suspense fallback={<div>Loading...</div>}><Login /></Suspense>}
+              />
+            }
+          />
+        </Route>
+        <Route path="*" element={<Suspense fallback={<div>Loading...</div>}><Home /></Suspense>} />
+      </Routes>
+    </div>
   );
 };
 
