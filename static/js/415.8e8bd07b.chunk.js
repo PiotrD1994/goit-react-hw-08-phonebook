9755b180411658ebf814cc96723fe790b8e4d963
PiotrD1994 <125481264+PiotrD1994@users.noreply.github.com>
2023-12-09 16:46:02 +0000

    Deploying to gh-pages from @ PiotrD1994/goit-react-hw-08-phonebook@b1225ed12f4a0136c919bc147a84ef3499db9d7f 🚀

---
+++ static/js/415.8e8bd07b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(3329);function c(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{style:{textAlign:"center"},children:"Welcome to Phonebook!"})})}}}]);
+//# sourceMappingURL=415.8e8bd07b.chunk.js.map
